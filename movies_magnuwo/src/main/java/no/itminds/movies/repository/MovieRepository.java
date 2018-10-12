124ea8fc4c5f79736099e1e3b79d6bc46fa29c91
magneoe <magneoe@gmail.com>
2018-10-12 13:40:59 +0200

    Fixed bug where pageable did not work properly

--- movies_magnuwo/src/main/java/no/itminds/movies/repository/MovieRepository.java
+++ movies_magnuwo/src/main/java/no/itminds/movies/repository/MovieRepository.java
@@ -12,6 +12,7 @@ import no.itminds.movies.model.Movie;
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
 	Movie findByTitle(String title);
-	@Query("SELECT m.comments FROM Movie m WHERE m.id = ?1")
+	@Query(value="SELECT m.comments FROM Movie m WHERE m.id = ?1", 
+			countQuery="SELECT count(c) FROM Movie m INNER JOIN m.comments c WHERE m.id = ?1")
 	Page<Comment> getComments(Long movieId, Pageable pageable);
 }
