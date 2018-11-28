1cbdd27a4492a07fa64ffb5d594e643f1432cf16
magneoe <magneoe@users.noreply.github.com>
2018-11-28 08:58:30 +0100

    Improved errorhandling

--- src/main/java/no/itminds/movies/service/impl/TokenAuthenticationService.java
+++ src/main/java/no/itminds/movies/service/impl/TokenAuthenticationService.java
@@ -7,16 +7,20 @@ import java.util.Date;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
 
 @Service
 public class TokenAuthenticationService {
-
+	static final Logger logger = LoggerFactory.getLogger(TokenAuthenticationService.class);
+	
 	static final long EXPIRATIONTIME = 1800_000; //30 mins 
 	private static final String SECRET = "ThisIsASecret";
 	public static final String TOKEN_PREFIX = "Bearer";
@@ -33,9 +37,14 @@ public class TokenAuthenticationService {
 		String token = request.getHeader(HEADER_STRING);
 		if (token != null) {
 			// parse the token.
-			String user = Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token.replace(TOKEN_PREFIX, "")).getBody()
+			String user = null;
+			try {
+				user = Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token.replace(TOKEN_PREFIX, "")).getBody()
 					.getSubject();
-
+			}
+			catch(MalformedJwtException mjwtex) {
+				logger.warn("Unable to parse JWT");
+			}
 			return user != null ? new UsernamePasswordAuthenticationToken(user, null, emptyList()) : null;
 		}
 		return null;
